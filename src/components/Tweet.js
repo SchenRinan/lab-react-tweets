d932a8a49a7e2733e45264f980ff52506fe5f3f5
Carlo Gabriel <jandrewschnell@gmail.com>
2023-07-21 08:38:15 +0200

    solved lab

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,33 +1,50 @@
+import Actions from "./Actions";
+import Message from "./Message";
+import ProfileImage from "./ProfileImage";
+import Timestamp from "./Timestamp";
+import User from "./User";
+
 function Tweet(props) {
   return (
     <div className="tweet">
-      <img
+
+
+      <ProfileImage image = { props.tweet.user.image }/>
+      {/* <img
         src= { props.tweet.user.image }
         className="profile"
         alt="profile"
-      />
+      /> */}
 
       <div className="body">
         <div className="top">
-          <span className="user">
+
+          <User 
+            name = { props.tweet.user.name } 
+            handle = { props.tweet.user.handle }
+            />
+          {/* <span className="user">
             <span className="name">{ props.tweet.user.name }</span>
             <span className="handle">@{ props.tweet.user.handle }</span>
-          </span>
+          </span> */}
 
-          <span className="timestamp">{ props.tweet.timestamp }</span>
+          <Timestamp timestamp = { props.tweet.timestamp }/>
+          {/* <span className="timestamp">{ props.tweet.timestamp }</span> */}
         </div>
 
-        <p className="message">
+        <Message message = { props.tweet.message }/>
+        {/* <p className="message">
           { props.tweet.message }
-        </p>
+        </p> */}
 
-        <div className="actions">
+        <Actions/>
+        {/* <div className="actions"> */}
           {/* Font Awesome icons */}
-          <i className="far fa-comment"></i>
+          {/* <i className="far fa-comment"></i>
           <i className="fas fa-retweet"></i>
           <i className="far fa-heart"></i>
           <i className="fas fa-share"></i>
-        </div>
+        </div> */}
       </div>
 
       <i className="fas fa-ellipsis-h"></i>
